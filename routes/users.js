23713b7d0176e5edbb9e78170834b0a9507ff0f0
march-dave <ysangy@gmail.com>
2016-06-16 22:51:57 -0700

    Cron Job add

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,12 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+
+  var CronJob = require('cron').CronJob;
+  new CronJob('* * * * * *', function() {
+    console.log('You will see this message every second');
+  }, null, true, 'America/Los_Angeles');
+
   res.send('respond with a resource');
 });
 

6ada76b6a6d509332c1e4c33ebbe06da4999c99e
march-dave <ysangy@gmail.com>
2016-06-17 00:54:13 -0700

    D3.js add

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+var d3 = require('d3'),
+  jsdom = require('jsdom');
+
+var document = jsdom.jsdom();
+  svg = d3.select(document.body).append('svg');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
